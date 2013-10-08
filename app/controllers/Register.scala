e4e989538d7bfae3f24b6494aa95aaa6bfc86a8c
Mark Wilson <mwilson@processor44.com>
2013-10-07 18:15:04 -0600

    Enhanced get and put to use the simple mutable map database

--- app/controllers/Register.scala
+++ app/controllers/Register.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import models._
 import play.api.mvc.Action
 import play.api.mvc.AnyContent
 import play.api.mvc.Controller
@@ -20,17 +21,30 @@ object Register extends Controller {
    * GET the customer's callback configuration.
    *  id is the customer id.
    */
-  def getCallbackregistration(id: Long) = Action.async { request =>
-    val f: Future[String] = Future { """{"id":1, "foo":"true"}""" }
-    f.map(s => Ok(s))
+  def getCallbackRegistration(id: Long) = Action.async { request =>
+    val f: Future[Option[String]] = Future {
+      val ocb = CallbackCatalog.catalog.get(id)
+      ocb.isDefined match {
+        case false => None
+        case _ => {
+          val j: JsValue = Json.toJson(ocb.get)
+          Some(Json.stringify(j))
+        }
+      }
+    }
+    f.map(os =>
+      os.isDefined match {
+        case true => Ok(os.get)
+        case _ => Ok("None Found for " + id)
+      })
   }
 
   /**
    * PUT the customer's callback configuration.
    *  id is the customer id.
    */
-  def putCallbackregistration(id: Long) = Action.async { request =>
-    val f: Future[(Boolean, String)] = Future { updateCallbackregistration(id, request) }
+  def putCallbackRegistration(id: Long) = Action.async { request =>
+    val f: Future[(Boolean, String)] = Future { updateCallbackRegistration(id, request) }
     f.map { bs =>
       {
         bs._1 match {
@@ -44,13 +58,29 @@ object Register extends Controller {
   /**
    * do the work of PUT registration
    */
-  def updateCallbackregistration(id: Long, request: Request[AnyContent]): (Boolean, String) = {
+  def updateCallbackRegistration(id: Long, request: Request[AnyContent]): (Boolean, String) = {
     val jsonBody: Option[JsValue] = request.body.asJson
     play.api.Logger.debug("putCallbackregistration " + id + " " + request.body)
     jsonBody match {
       case None => { (false, "Could not resolve request body as json.  Do you have the 'Content-type' header set to 'application/json'? " + request.body) }
-      case _ => { (true, Json.stringify(jsonBody.get)) } // gives back received json for now
+      case _ => {
+        try {
+          val callback: Callback = resolveCallback(jsonBody.get)
+          CallbackCatalog.saveCallback(callback)
+          (true, Json.stringify(jsonBody.get))
+        } catch {
+          case e: Exception => (false, e.getMessage)
+        }
+      }
     }
+  }
+
+  /** */
+  def resolveCallback(j: JsValue): Callback = {
+    val id: Long = (j \ "id").as[Long]
+    val url: String = (j \ "url").as[String]
+    val r: Callback = new Callback(id, url)
+    r
   }
 
 }
