0d2825c4a547fd0b0305f51053a770c787320de0
Mark Wilson <mwilson@processor44.com>
2013-10-08 17:16:00 -0600

    Built simple objects for cataloging Consumers and Tokens for simulation

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -1,12 +1,12 @@
 package models
 
+import util._
 import play.api.libs.json._
 
 /**
  * 
  */
-object Models {
-}
+object Models {}
 
 trait JsonParsable[T] {
   def toJsValue(obj: T): JsValue
@@ -46,3 +46,31 @@ object CallbackCatalog {
     catalog.put(callback.customerId, callback.url)
   }
 }
+
+/*
+RFC 1738 encoded consumer key (does not change)	xvz1evFS4wEEPTGEFPHBog
+RFC 1738 encoded consumer secret (does not change)	L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg
+Bearer token credentials:	xvz1evFS4wEEPTGEFPHBog:L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg
+Base64 encoded bearer token credentials:  eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw==
+*/
+case class Consumer(id: Long, name: String, key: String, secret: String) {
+  def buildCredentials: String = Consumer.buildCredentialsFromKeySecret(key, secret)
+//  def buildCredentialsBase64: String = {
+//    //org.apache.commons.codec.binary.Base64.encodeBase64(x$1)
+//    getCredentials
+//  }
+}
+object Consumer {
+  def buildCredentials(consumer: Consumer): String = { buildCredentialsFromKeySecret(consumer.key, consumer.secret) }
+  def buildCredentialsFromKeySecret(k: String, sec: String) = { k + " :" + sec }
+  def splitCredentials(cred: String) = { 
+    val sa = cred.split(":") 
+    (sa(0), sa(1))
+  }
+}
+object ConsumerCatalog {
+   val consumers: scala.collection.mutable.Map[Long, Consumer] = scala.collection.mutable.Map((1L -> Consumer(1L, "Simulator Co.", "vcncb4TTuPPTEGLOSKIex", "L9yyTYyQg9ieKLOPhWolMNVvKUUw8iE7777777yOg")))
+   val tokens: scala.collection.mutable.Map[Long, Option[String]] = scala.collection.mutable.Map((1L -> None))
+}
+
+
