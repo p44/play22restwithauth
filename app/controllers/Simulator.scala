76c984a3d26af0b934ff4caea493e6e1d099da38
Mark Wilson <mwilson@processor44.com>
2013-10-07 18:13:49 -0600

    Simulator controller with callback added

---
+++ app/controllers/Simulator.scala
@@ -0,0 +1,32 @@
+package controllers
+
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import play.api.mvc.Controller
+import play.api.mvc.Request
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json._
+
+/**
+ * Simulates a Consumer of the registration service
+ */
+object Simulator extends Controller {
+  
+  /**
+   * Simulated callback
+   * PUT /simulator/mycallback
+   */
+  def putMyCallback = Action { request =>
+    val jsonBody: Option[JsValue] = request.body.asJson
+    play.api.Logger.debug("putMyCallback " + request.body)
+    jsonBody match {
+      case None => { BadRequest("Could not resolve request body as json.  Do you have the 'Content-type' header set to 'application/json'? " + request.body) }
+      case _ => { 
+        play.api.Logger.info("putMyCallback success: " + jsonBody.get)
+        Ok("Ack") 
+      }
+    }
+  }
+
+}
