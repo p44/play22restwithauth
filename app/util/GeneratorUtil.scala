dce16ec8c8dbb2c2fc27ab1bd117ed33bea8da2a
Mark Wilson <mwilson@processor44.com>
2013-10-11 10:52:10 -0600

    Default param "" for makeStringOfPossibleChars

--- app/util/GeneratorUtil.scala
+++ app/util/GeneratorUtil.scala
@@ -10,15 +10,13 @@ object GeneratorUtil {
   val secRandom: java.security.SecureRandom = new java.security.SecureRandom()
 
   /** 32 char bearer token per spec RFC6750 (http://http://tools.ietf.org/html/rfc6750) */
-  def generateBearerToken: String = {
-    makeStringOfPossibleChars(32, "")
-  }
+  def generateBearerToken: String = { makeStringOfPossibleChars(32) }
 
   /**
    * uses recursion to assemble a String of random characters from POSSIBLE_CHARS
    */
-  def makeStringOfPossibleChars(len: Int, tokenBase: String): String = {
-    (len == 0) match {
+  def makeStringOfPossibleChars(len: Int, tokenBase: String = ""): String = {
+    (len <= 0) match {
       case true => tokenBase
       case _ => {
         val newBase = tokenBase + POSSIBLE_CHARS(secRandom.nextInt(POSSIBLE_CHARS.length))
