2bb59694eea0748917ec66d211d151fbe261f710
Mark Wilson <mwilson@processor44.com>
2013-10-11 18:26:19 -0600

    Get the simulated consumer from the server and display the name in the view

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -3,4 +3,5 @@
 /** Controllers */
 angular.module('simulator.controllers', ['simulator.services']).
 controller('ConsumerCtrl', function ($scope, $http, consumerModel) {  
+	$scope.consumer_simulated = consumerModel.getSimulatedConsumer($scope, $http);
 });
--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -5,21 +5,17 @@
 /** imAlive service, provides customers (could also be loaded from server) */
 angular.module('simulator.services', []).
 service('consumerModel', function () {
-    var getConsumer = function ($scope, $http) {
+    var getSimulatedConsumer = function ($scope, $http) {
     	$http({method: 'GET', url: '/consumer/simulated'}). // , headers: {'foo': 'bar'}}).
     	  success(function(data, status, headers, config) {
-    	    // this callback will be called asynchronously when the response is available
-    		// data: [object Object],[object Object] from [{"id":1,"name":"Supreme ATMs"},{"id":2,"name":"Terminal Specialists"}]
     		console.log('GET /consumer/simulated')
-    		for (var i = 0; i < data.length; i++) {
-    		  console.log(data[i]);
-    		}
-    		$scope.consumer = data;
+    		console.log(data);
+    		$scope.consumer_simulated = data;
     	  }).
     	  error(function(data, status, headers, config) {
     		console.log('GET /consumer/simulated ERROR ' + status)
-    		$scope.consumer = [{id: 0, name: "None"}];
+    		$scope.consumer_simulated = [{id: 0, name: "None"}];
     	  });
     };
-    return { getConsumer: getConsumer };
+    return { getSimulatedConsumer: getSimulatedConsumer };
 });
