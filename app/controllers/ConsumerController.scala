b20711c22d74b6949e45be31b2bb5ef1ab3360ce
Mark Wilson <mwilson@processor44.com>
2013-10-14 11:13:55 -0600

    get simulated controller now calls Models convenience function ConsumerCatalog.getConsumerSimulated

--- app/controllers/ConsumerController.scala
+++ app/controllers/ConsumerController.scala
@@ -18,7 +18,7 @@ object ConsumerController extends Controller {
   
   /** Get the already created simulated consumer for simulation purposes - no security */
   def getConsumerSimulated = Action {
-    val oc: Option[Consumer] = ConsumerCatalog.consumers.get(1L)
+    val oc: Option[Consumer] = ConsumerCatalog.getConsumerSimulated
     oc.isDefined match {
       case false => InternalServerError("The system failed to provide a simulated consumer")
       case true => { Ok(oc.get.toGetJson) } // json without the key and secret
