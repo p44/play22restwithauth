a4962a6fdcc68dc6f8643582567b913e6b171681
Mark Wilson <mwilson@processor44.com>
2013-10-14 11:12:58 -0600

    Add function to get simulated consumer from constant id value, renamed authorization functions, added spec test for ConsumerCatalog.authorizeConsumerCredential

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -48,10 +48,10 @@ object CallbackCatalog {
 }
 
 /*
-RFC 1738 encoded consumer key (does not change)	xvz1evFS4wEEPTGEFPHBog
-RFC 1738 encoded consumer secret (does not change)	L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg
-Bearer token credentials:	xvz1evFS4wEEPTGEFPHBog:L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg
-Base64 encoded bearer token credentials:  eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw==
+RFC 1738 encoded consumer key (does not change)	vcncb4TTuPPTEGLOSKIex
+RFC 1738 encoded consumer secret (does not change)	L9yyTYyQg9ieKLOPhWolMNVvKUUw8iE7777777yOg
+Bearer token credentials:	vcncb4TTuPPTEGLOSKIex:L9yyTYyQg9ieKLOPhWolMNVvKUUw8iE7777777yOg
+Base64 encoded bearer token credentials:  ---
 */
 case class Consumer(id: Long, name: String, key: String, secret: String) {
   def buildCredentials: String = Consumer.buildCredentialsFromKeySecret(key, secret)
@@ -65,7 +65,7 @@ case class Consumer(id: Long, name: String, key: String, secret: String) {
 }
 object Consumer {
   def buildCredentials(consumer: Consumer): String = { buildCredentialsFromKeySecret(consumer.key, consumer.secret) }
-  def buildCredentialsFromKeySecret(k: String, sec: String) = { k + " :" + sec }
+  def buildCredentialsFromKeySecret(k: String, sec: String) = { k + ":" + sec }
   /** returns tuple (key, secret) */
   def splitCredentials(cred: String): (String, String) = { 
     val sa = cred.split(":") 
@@ -73,8 +73,13 @@ object Consumer {
   }
 }
 object ConsumerCatalog {
-   val consumers: scala.collection.mutable.Map[Long, Consumer] = scala.collection.mutable.Map((1L -> Consumer(1L, "Simulator Co.", "vcncb4TTuPPTEGLOSKIex", "L9yyTYyQg9ieKLOPhWolMNVvKUUw8iE7777777yOg")))
-   val tokens: scala.collection.mutable.Map[Long, Option[String]] = scala.collection.mutable.Map((1L -> None))
+   val ID_SIMULATED_CONSUMER: Long = 1L
+   val consumers: scala.collection.mutable.Map[Long, Consumer] = scala.collection.mutable.Map((ID_SIMULATED_CONSUMER -> Consumer(1L, "Simulator Co.", "vcncb4TTuPPTEGLOSKIex", "L9yyTYyQg9ieKLOPhWolMNVvKUUw8iE7777777yOg")))
+   val tokens: scala.collection.mutable.Map[Long, Option[String]] = scala.collection.mutable.Map((ID_SIMULATED_CONSUMER -> None))
+   
+   def getConsumerSimulated: Option[Consumer] = {
+     ConsumerCatalog.consumers.get(ID_SIMULATED_CONSUMER)
+   }
    
    def getConsumerByCredentials(cred: String): Option[Consumer] = {
      val filtered = consumers.values.filter(x => x.buildCredentials == cred)
@@ -84,14 +89,14 @@ object ConsumerCatalog {
      }
    }
    
-   def authenticateToken(t: String): Option[String] = {
+   def authorizeToken(t: String): Option[String] = {
      tokens.values.exists(x => x.getOrElse("***") == t) match {
        case false => None
        case _ => { Some(t) }
      }
    }
    
-   def authenticateConsumerCredential(cred: String): Option[String] = {
+   def authorizeConsumerCredential(cred: String): Option[String] = {
      consumers.values.exists(x => x.buildCredentials == cred) match {
        case false => None
        case _ => { Some(cred) }
--- test/models/ModelsSpec.scala
+++ test/models/ModelsSpec.scala
@@ -17,10 +17,19 @@ object ModelsSpec extends Specification {
     "ConsumerCatalog.tokens" in {
       val bearerToken = GeneratorUtil.generateBearerToken
       ConsumerCatalog.tokens.put(99L, Some(bearerToken))
-      ConsumerCatalog.authenticateToken(bearerToken) mustNotEqual None
+      ConsumerCatalog.authorizeToken(bearerToken) mustNotEqual None
       val g: Option[String] = ConsumerCatalog.tokens.get(99L).getOrElse(None)
       g mustNotEqual None
       g.get mustEqual bearerToken
+    }
+    "ConsumerCatalog.authorizeConsumerCredential" in {
+      // vcncb4TTuPPTEGLOSKIex:L9yyTYyQg9ieKLOPhWolMNVvKUUw8iE7777777yOg
+      val oc: Option[Consumer] = ConsumerCatalog.getConsumerSimulated
+      oc mustNotEqual None
+      val cred = oc.get.buildCredentials
+      println("ConsumerCatalog.authorizeConsumerCredential cred " + cred)
+      val authorizedCred = ConsumerCatalog.authorizeConsumerCredential(cred)
+      authorizedCred mustNotEqual None
     }
   }
 
